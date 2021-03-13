338376eb2695ea15aecb91b129eec36b1c04787e
aozora <marcellop@protonmail.com>
2021-03-13 19:06:33 +0100

    feat: Lab #1 Sketchin Home - menu toggle, gsap animation MenuPanel

--- src/components/MenuPanel.js
+++ src/components/MenuPanel.js
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, { useRef, useEffect, useMemo } from 'react';
+import gsap from 'gsap';
 import styles from './MenuPanel.module.scss';
+import { useAppState } from '../AppContext';
 
 const MenuPanel = () => {
-  return <section className={styles.MenuPanel}></section>;
+  const { state } = useAppState();
+  const menuPanelRef = useRef();
+
+  const timeline = useMemo(() => {
+    return gsap.timeline({ paused: true });
+  }, []);
+
+  useEffect(() => {
+    timeline.fromTo(
+      menuPanelRef.current,
+      {
+        x: '-100%'
+        // y: 0
+      },
+      {
+        x: 0,
+        // y: 0,
+        duration: 1.2,
+        ease: 'expo.inOut'
+      }
+    );
+  }, [timeline]);
+
+  useEffect(() => {
+    if (menuPanelRef.current) {
+      if (state.menuIsOpen) {
+        timeline.play();
+      } else {
+        timeline.reverse();
+      }
+    }
+  }, [menuPanelRef, state.menuIsOpen, timeline]);
+
+  return (
+    <section ref={menuPanelRef} className={`${styles.MenuPanel} ${state.menuIsOpen ? styles.MenuPanelIsOpen : ''}`}>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </section>
+  );
 };
 
 export default MenuPanel;
