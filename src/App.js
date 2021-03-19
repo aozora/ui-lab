fe8b87ca55eaebccff8ccdeb9b18c78e7998bcfa
aozora <marcellop@protonmail.com>
2021-03-19 21:48:41 +0100

    feat: Lab #1 Sketchin Home - contents and scroll effects

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Hero from './components/Hero';
 import MenuPanel from './components/MenuPanel';
 import { AppProvider } from './AppContext';
 import Services from './components/Services';
+import { useEffect, useMemo, useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import servicesStyles from './components/Services.module.scss';
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   return (
@@ -14,6 +20,14 @@ function App() {
 
         <Hero />
         <Services />
+
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </div>
     </AppProvider>
   );
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './Services.module.scss';
+import gsap from 'gsap';
+import servicesStyles from './Services.module.scss';
 
-const Services = () => (
-  <article className={styles.Services}>
-    <div>
-      <h1>Lorem Ipsum sin ut dolorem</h1>
-      <footer>
-        <p>Vero omnis sed recusandae voluptas repellat in omnis. Et maiores cumque incidunt voluptatum est.</p>
-        <p>Vero omnis sed recusandae voluptas repellat in omnis. Et maiores cumque incidunt voluptatum est.</p>
-        <p>Vero omnis sed recusandae voluptas repellat in omnis. Et maiores cumque incidunt voluptatum est.</p>
-      </footer>
-    </div>
-  </article>
-);
+const Services = () => {
+  const servicesRef = useRef();
+
+  useEffect(() => {
+    if (servicesRef.current) {
+      gsap.to(servicesRef.current, {
+        scrollTrigger: {
+          trigger: servicesRef.current,
+          pin: false, // pin the trigger element while active
+          start: 'top center', // when the top of the trigger hits the bottom of the viewport
+          end: 'bottom top', // end after scrolling 500px beyond the start
+          // toggleClass: servicesStyles.ThemeDark,
+          toggleClass: { targets: 'body', className: 'theme-dark' /*servicesStyles.ThemeDark */ },
+          markers: true
+        }
+      });
+    }
+  }, []);
+
+  return (
+    <article ref={servicesRef} className={styles.Services}>
+      <div>
+        <h1>Lorem Ipsum sin ut dolorem</h1>
+        <footer>
+          <p>Vero omnis sed recusandae voluptas repellat in omnis. Et maiores cumque incidunt voluptatum est.</p>
+          <p>Vero omnis sed recusandae voluptas repellat in omnis. Et maiores cumque incidunt voluptatum est.</p>
+          <p>Vero omnis sed recusandae voluptas repellat in omnis. Et maiores cumque incidunt voluptatum est.</p>
+        </footer>
+      </div>
+    </article>
+  );
+};
 
 export default Services;
