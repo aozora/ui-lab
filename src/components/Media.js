1eefea29b8b7e38df6fb713aa3fbeea223424734
aozora <marcellop@protonmail.com>
2021-03-21 10:09:09 +0100

    feat: Lab #1 Sketchin Home - added media with effects

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import gsap from 'gsap';
 import styles from './Media.module.scss';
 
 const Media = () => {
+  /**
+   * animate on mouse enter/leave
+   * Ref: https://tympanus.net/Development/ImageStackGrid/
+   *      https://github.com/codrops/ImageStackGrid/blob/main/src/js/demo1/galleryItem.js
+   * @type {function(*): void}
+   */
+  const onMouseEnterFn = useCallback(event => {
+    gsap
+      .timeline({ defaults: { duration: 1, ease: 'expo' } })
+      .to(event.target.parentElement, { scale: 0.95 })
+      .to(event.target, { scale: 1.2 }, 0);
+  }, []);
+
+  const onMouseLeaveFn = useCallback(event => {
+    gsap.timeline({ defaults: { duration: 1, ease: 'expo' } }).to([event.target, event.target.parentElement], { scale: 1 });
+  }, []);
+
   return (
     <div className={styles.MediaContainer}>
       <article className={styles.Media}>
         <img
+          onMouseEnter={onMouseEnterFn}
+          onMouseLeave={onMouseLeaveFn}
           sizes="(max-width: 1400px) 100vw, 1400px"
           srcSet="
 /media/pexels-gabriel-crişan-2119953_uq92wr_c_scale,w_320.jpg 320w,
@@ -18,6 +38,8 @@ const Media = () => {
 
       <article className={styles.Media}>
         <img
+          onMouseEnter={onMouseEnterFn}
+          onMouseLeave={onMouseLeaveFn}
           sizes="(max-width: 1400px) 100vw, 1400px"
           srcSet="
 /media/pexels-charles-pragnell-2524164_sjhz9v_c_scale,w_320.jpg 320w,
@@ -30,6 +52,8 @@ const Media = () => {
 
       <article className={styles.Media}>
         <img
+          onMouseEnter={onMouseEnterFn}
+          onMouseLeave={onMouseLeaveFn}
           sizes="(max-width: 1400px) 100vw, 1400px"
           srcSet="
 /media/pexels-kirsten-bu%CC%88hne-1521304_sl7lyr_c_scale,w_320.jpg 320w,
@@ -42,6 +66,8 @@ const Media = () => {
 
       <article className={styles.Media}>
         <img
+          onMouseEnter={onMouseEnterFn}
+          onMouseLeave={onMouseLeaveFn}
           sizes="(max-width: 1400px) 100vw, 1400px"
           srcSet="
 /media/pexels-mermek-am-1573324_qhspye_c_scale,w_320.jpg 320w,
@@ -54,6 +80,8 @@ const Media = () => {
 
       <article className={styles.Media}>
         <img
+          onMouseEnter={onMouseEnterFn}
+          onMouseLeave={onMouseLeaveFn}
           sizes="(max-width: 1400px) 100vw, 1400px"
           srcSet="
 /media/pexels-pixabay-57416_mogsye_c_scale,w_320.jpg 320w,
@@ -66,6 +94,8 @@ const Media = () => {
 
       <article className={styles.Media}>
         <img
+          onMouseEnter={onMouseEnterFn}
+          onMouseLeave={onMouseLeaveFn}
           sizes="(max-width: 1400px) 100vw, 1400px"
           srcSet="
 /media/pexels-pixabay-357141_krjb5x_c_scale,w_320.jpg 320w,
